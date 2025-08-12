15ecd5a321578ad0b0f6c40068890156245a19b4
Rares Ifrim <raresifrim@Raress-MacBook-Air.local>
2025-08-12 22:43:01 +0300

    first commit

---
+++ src/bloom.rs
@@ -0,0 +1,163 @@
+
+use ::xoodoo_hash::xoodoo_hash::{xoodoo_state::{XoodooStateNC}, XoodooHash};
+pub struct Bloom1X {
+    /// number of hashes
+    k: usize,
+    /// number of rows in filter
+    l: usize,
+    /// width of row in bytes
+    w: usize,
+    /// number of bits to be extracted from hash for computing the row index
+    row_bits: usize,
+    /// number of bits to be extracted from hash for each sub-hash
+    hash_bits: usize,
+    /// memory representation of filter
+    filter: Box<[Box<[u8]>]>
+}
+
+/// contains the info of a query given a hash digest
+struct QueryResult {
+    /// gets the values of each sub-hash
+    bits: Vec<u8>,
+    /// gets the index where the bit is found inside a row
+    bit_indexes: Vec<usize>,
+    /// gets the row index inside the filter
+    row_index: usize,
+    /// gets the bitwise and result between the bits
+    and_result: u8
+}
+
+impl Bloom1X {
+    /// a filter represents a 2D array 
+    /// w -> size of a row
+    /// l -> number of rows
+    /// h -> size of hash function output in bits
+    pub fn new(k: usize, l: usize, w: usize, h:usize) -> Self {
+        //some decent numbers should be provided
+        assert!(w >= 32 && w % 8 == 0 && w <= 256);
+        assert!(l >= 1024 && (l & (l - 1)) == 0 && l < u32::MAX as usize);
+        assert!(k >= 2);
+        assert!(h > 64);
+
+        //number of bits needed to get a row inside the filter
+        let row_bits = l.checked_ilog2().unwrap() as usize;
+        assert!(row_bits < h);
+        assert!((h - row_bits) % k == 0);
+        let hash_bits = (h - row_bits) / k;
+
+        let mut filter = vec![];
+        for i in 0..l {
+            let row = vec![0 as u8; w/8];
+            filter.push(row.into_boxed_slice());
+        }
+        let filter =filter.into_boxed_slice();
+        Self { k, l, w, row_bits, hash_bits, filter }
+    }
+
+    pub fn clear(&mut self) {
+        for i in 0..self.l {
+            for j in 0..self.w/8 {
+                self.filter[i][j] = 0;
+            }
+        }
+    }
+
+    /// the query functions check if a provided key is member of the filter
+    /// returns 0x1 if the provided bytes were found inside the filter and 0x0 otherwise
+    pub fn query_bytes(&self, bytes: &[u8]) -> u8 {
+        assert!(bytes.len() * 8 <= 96); //can query keys of maximum 96-bits size
+        let mut hasher = XoodooHash::<XoodooStateNC>::new_from_bytes(bytes);
+        hasher.permute_nc();
+        let digest = hasher.digest_nc();
+        let query_result = self.parse_hash(&digest);
+        query_result.and_result
+    }
+
+    /// returns 0x1 if the provided u32 was found inside the filter and 0x0 otherwise
+    pub fn query_u32(&self, bytes: u32) -> u8 {
+        let mut hasher = XoodooHash::<XoodooStateNC>::new_from_u32(bytes);
+        hasher.permute_nc();
+        let digest = hasher.digest_nc();
+        let query_result = self.parse_hash(&digest);
+        query_result.and_result
+    }
+
+    /// returns 0x1 if the provided u64 was found inside the filter and 0x0 otherwise
+    pub fn query_u64(&self, bytes: u64) -> u8 {
+        let mut hasher = XoodooHash::<XoodooStateNC>::new_from_u64(bytes);
+        hasher.permute_nc();
+        let digest = hasher.digest_nc();
+        let query_result = self.parse_hash(&digest);
+        query_result.and_result
+    }
+
+    /// given a digest obtained from the hash function, outputs the query info
+    #[inline]
+    fn parse_hash(&self, digest: &[u32; 3]) -> QueryResult {
+        let row_index = (digest[2] >> (32 - self.row_bits)) as usize; 
+        let high_bits = digest[2] << self.row_bits;
+
+        let digest:u128 = digest[0] as u128 | ((digest[1] as u128) << 32) | (high_bits as u128) << (64 - self.row_bits);
+        //println!("concatenated digest -> {:X}", digest);
+        let row = &self.filter[row_index];
+
+        let mut bits= vec![];
+        let mut bit_indexes= vec![];
+        let mut and_result: u8 = 1;
+        for i in 0..self.k {
+            let bit_index = (digest >> (i * self.hash_bits)) as usize % self.w;
+            let byte = row[bit_index/8];
+            let bit = (byte >> (bit_index % 8)) & 0x1;
+            //println!("bit_value= {bit} in byte {:X} @ position {bit_index} @ row {row_index}", byte);
+            bits.push(bit);
+            bit_indexes.push(bit_index);
+            and_result &= bit;   
+        }
+        QueryResult { bits, bit_indexes, row_index, and_result }
+    }
+
+    /// given a query result, update the filter
+    #[inline]
+    fn update_filter(&mut self, qr: QueryResult) {
+        let row = &mut self.filter[qr.row_index];
+        for i in 0..self.k {
+            let bit_index = qr.bit_indexes[i];
+            let mut byte = row[bit_index/8];
+            byte |= 1 << (bit_index % 8);
+            row[bit_index/8] = byte;
+        }
+    }
+
+    /// this functions queris the memebership returning the true/false response
+    /// and also updates the filter with the new bits 
+    pub fn query_and_set_bytes(&mut self, bytes: &[u8]) -> u8 {
+        assert!(bytes.len() * 8 <= 96); //can query keys of maximum 96-bits size
+        let mut hasher = XoodooHash::<XoodooStateNC>::new_from_bytes(bytes);
+        hasher.permute_nc();
+        let digest = hasher.digest_nc();
+        let query_result = self.parse_hash(&digest);
+        let result = query_result.and_result;
+        self.update_filter(query_result);
+        result
+    }
+
+    pub fn query_and_set_u32(&mut self, bytes: u32) -> u8 {
+        let mut hasher = XoodooHash::<XoodooStateNC>::new_from_u32(bytes);
+        hasher.permute_nc();
+        let digest = hasher.digest_nc();
+        let query_result = self.parse_hash(&digest);
+        let result = query_result.and_result;
+        self.update_filter(query_result);
+        result    
+    }
+
+    pub fn query_and_set_u64(&mut self, bytes: u64) -> u8 {
+        let mut hasher = XoodooHash::<XoodooStateNC>::new_from_u64(bytes);
+        hasher.permute_nc();
+        let digest = hasher.digest_nc();
+        let query_result = self.parse_hash(&digest);
+        let result = query_result.and_result;
+        self.update_filter(query_result);
+        result   
+    }
+}
---
+++ src/lib.rs
@@ -0,0 +1,134 @@
+pub mod bloom;
+
+#[cfg(test)]
+mod tests {
+    use crate::bloom::Bloom1X;
+
+    #[test]
+    fn filter_membership() {
+        let mut bloom_filter = Bloom1X::new(4, u16::MAX as usize + 1, 96, 96);
+
+        //generate first 2^16 numbers
+        for i in 0..(u16::MAX as u32 + 1) {
+            assert!(bloom_filter.query_and_set_u32(i) == 0);
+        }
+
+        //check filter for current members
+        for i in 0..(u16::MAX as u32 + 1) {
+            assert!(bloom_filter.query_u32(i) == 1);
+        }
+
+        //check next int outside filter
+        assert!(bloom_filter.query_u32(u16::MAX as u32 + 1) == 0)
+    }
+
+    #[test]
+    fn false_positive_768KB_small() {
+        let mut bloom_filter = Bloom1X::new(4, u16::MAX as usize + 1, 96, 96);
+
+        //generate first 2^16 numbers
+        for i in 0..(u16::MAX as u32 + 1) {
+            assert!(bloom_filter.query_and_set_u32(i) == 0);
+        }
+
+        //check filter for next set of numbers
+        let mut count = 0;
+        for i in (u16::MAX as u32 + 1)..(2 * u16::MAX as u32) {
+            count += bloom_filter.query_and_set_u32(i);
+        }
+        let k = 4.0;
+        let m = 96.0 * u16::MAX as f64;
+        let n = 2.0 * u16::MAX as f64;
+        let p = f64::powf(1.0 - f64::exp(-k / (m / n)), k);
+
+        println!(
+            "False Positives in {} elements = {}",
+            2 * u16::MAX as u32,
+            count
+        );
+        println!(
+            "False Positives rate = {}",
+            count as f64 / (2.0 * u16::MAX as f64)
+        );
+        println!("Computed False Positive rate = {}", p);
+    }
+
+    #[test]
+    fn false_positive_768KB_large() {
+        let mut bloom_filter = Bloom1X::new(4, u16::MAX as usize + 1, 96, 96);
+
+        let mut count: usize = 0;
+        for i in 0..1_000_000 {
+            count += bloom_filter.query_and_set_u64(i + 0xDEADBEEF) as usize;
+        }
+
+        for i in 1_000_000..2_000_000 {
+            count += bloom_filter.query_and_set_u64(i + 0xDEADBEEF) as usize;
+        }
+        let k = 4.0;
+        let m = 96.0 * u16::MAX as f64;
+        let n = 2000000.0;
+        let p = f64::powf(1.0 - f64::exp(-k / (m / n)), k);
+
+        println!("False Positives in {} elements = {}", 2000000, count);
+        println!("False Positives rate = {}", count as f64 / 2000000.0);
+        println!("Computed False Positive rate = {}", p);
+    }
+
+    #[test]
+    fn false_positive_36Kb_small() {
+        let mut bloom_filter = Bloom1X::new(2, 1024, 32, 96);
+
+        let mut count: usize = 0;
+        //test with 4000 elements
+        for i in 0..4000 {
+            count += bloom_filter.query_and_set_u32(i) as usize;
+        }
+
+        //check filter for next set of numbers
+        for i in 4000..8000 {
+            count += bloom_filter.query_and_set_u32(i) as usize;
+        }
+
+        println!(
+            "False Positives in {} elements = {}",
+            2 * u16::MAX as u32,
+            count
+        );
+        println!(
+            "False Positives rate = {}",
+            count as f64 / (2.0 * u16::MAX as f64)
+        );
+
+        bloom_filter.clear();
+
+        let mut count: usize = 0;
+        //test with 4000 elements
+        for i in 0..4000 {
+            count += bloom_filter.query_and_set_u64(i + 0xDEADBEEF) as usize;
+        }
+
+        //check filter for next set of numbers
+        for i in 4000..8000 {
+            count += bloom_filter.query_and_set_u64(i + 0xDEADBEEF) as usize;
+        }
+
+        println!(
+            "False Positives 2 in {} elements = {}",
+            2 * u16::MAX as u32,
+            count
+        );
+        println!(
+            "False Positives 2 rate = {}",
+            count as f64 / (2.0 * u16::MAX as f64)
+        );
+
+        let k = 2.0;
+        let m = 36.0 * 1024.0; //FPGA BRAM block
+        let n = 4000.0;
+        let p = f64::powf(1.0 - f64::exp(-k / (m / n)), k);
+        println!("Computed False Positive rate = {}", p);
+    }
+
+    
+}
